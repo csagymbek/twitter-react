2da21d9fd009cf5a4200b2b22b1088d9e9bd742c
csagymbek <csagymbek@gmail.com>
2020-11-22 23:08:32 -0800

    first commit

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "../styles/Widgets.css";
+import SearchIcon from "@material-ui/icons/Search";
+import {
+  TwitterTweetEmbed,
+  TwitterShareButton,
+  TwitterTimelineEmbed,
+} from "react-twitter-embed";
 
 export default function Widgets() {
-  return <div className="widgets">
-    
-  </div>;
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening?</h2>
+        <TwitterTweetEmbed tweetId={"463440424141459456"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="Christopher Johnson"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton url="https://www.facebook.com/" />
+      </div>
+    </div>
+  );
 }
---
+++ src/configs/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV-q2agHUzYZO7w3OABsnWNkKmU_D2No4",
+  authDomain: "twitter-react-dc883.firebaseapp.com",
+  databaseURL: "https://twitter-react-dc883.firebaseio.com",
+  projectId: "twitter-react-dc883",
+  storageBucket: "twitter-react-dc883.appspot.com",
+  messagingSenderId: "1067819295767",
+  appId: "1:1067819295767:web:5ff10993833ae4796be852",
+  measurementId: "G-TJ75D9YYV2",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const db = firebaseApp.firestore();
